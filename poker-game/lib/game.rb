f8bc69867ffd73c79be23fa8b232067d5f6dc5bf
Michael Lombardi <mikelombardi4332@gmail.com>
2024-03-28 20:38:37 -0500

    added winning functions to game.rb to check for all winning hand possibilites

--- poker-game/lib/game.rb
+++ poker-game/lib/game.rb
@@ -22,9 +22,68 @@ class PokerGame
 
     @players.each do |player|
       current_hand = player.hand
+      hand_rank = evaluate_hand_rank(current_hand)
+
+      if best_hand.nil? || hand_rank > best_hand
+        best_hand = hand_rank
+        winning_player = player
+      end
     end
 
     winning_player
+  end
+
+  def evaluate_hand_rank(hand)
+    # Count occurrences of ranks and suits
+    ranks_count = Hash.new(0)
+    suits_count = Hash.new(0)
+
+    hand.each do |card|
+      rank, suit = card.split(' of ')
+      ranks_count[rank] += 1
+      suits_count[suit] += 1
+    end
+
+    # Check for different hand ranks
+    if has_straight?(ranks_count) && has_flush?(suits_count)
+      return 8 # Straight Flush
+    elsif has_n_of_a_kind?(ranks_count, 4)
+      return 7 # Four of a Kind
+    elsif has_n_of_a_kind?(ranks_count, 3) && has_n_of_a_kind?(ranks_count, 2)
+      return 6 # Full House
+    elsif has_flush?(suits_count)
+      return 5 # Flush
+    elsif has_straight?(ranks_count)
+      return 4 # Straight
+    elsif has_n_of_a_kind?(ranks_count, 3)
+      return 3 # Three of a Kind
+    elsif count_pairs(ranks_count) == 2
+      return 2 # Two Pair
+    elsif count_pairs(ranks_count) == 1
+      return 1 # One Pair
+    else
+      return 0 # High Card
+    end
+  end
+
+  def has_straight?(ranks_count)
+    sorted_ranks = ranks_count.keys.sort_by { |rank| Deck::RANKS.index(rank) }
+    sorted_ranks.each_cons(5) do |hand_ranks|
+      return true if (Deck::RANKS.index(hand_ranks.last) - Deck::RANKS.index(hand_ranks.first)) == 4
+    end
+    false
+  end
+
+  def has_flush?(suits_count)
+    suits_count.any? { |_, count| count == 5 }
+  end
+
+  def has_n_of_a_kind?(ranks_count, n)
+    ranks_count.any? { |_, count| count == n }
+  end
+
+  def count_pairs(ranks_count)
+    ranks_count.values.count { |count| count == 2 }
   end
 
   def bet(player, amount)
--- poker-game/lib/player.rb
+++ poker-game/lib/player.rb
@@ -1,10 +1,21 @@
 class Player
-    attr_accessor :name, :hand, :chips
-  
-    def initialize(name = "Player")
-      @name = name
-      @hand = [] #implement hand from deck
-      @chips = 100 # Starting chips
-    end
+  attr_accessor :name, :hand, :chips
+
+  def initialize(name = "Player")
+    @name = name
+    @hand = []
+    @chips = 100
   end
-  
+
+  def display_hand
+    @hand.join(", ")
+  end
+
+  def discard(indexes)
+    indexes.each { |index| @hand.delete_at(index) }
+  end
+
+  def receive(cards)
+    @hand.concat(cards)
+  end
+end
